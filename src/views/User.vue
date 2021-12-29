ddd7bbef4eac761022a03aec83f3d43ce5e32234
Your Namevladbond00111 git config --global user.name Your <vladbond00111@gmail.com>
2021-12-29 16:16:00 +0200

    commit for rewiew

---
+++ src/views/User.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="user">
+    <UserInfo :userInfo="userInfo"></UserInfo>
+  </div>
+</template>
+
+<script>
+import store from '../store'
+import UserInfo from '@/components/UserInfo.vue'
+
+export default {
+  name: 'News',
+  components: {
+    UserInfo
+    // NewsCard
+  },
+  data () {
+    return {
+      test: null
+    }
+  },
+  computed: {
+    userInfo() {
+      return store.state.news.userInfo
+    },
+    userFeed() {
+      return store.state.news.userFeed
+    }
+  },
+  mounted() {
+    store.dispatch('news/getUserInfo', this.$route.params.id)
+    // store.dispatch('news/getUserFeed', this.$route.params.id)
+  },
+  methods: {
+    getData () {
+    }
+  }
+}
+</script>
